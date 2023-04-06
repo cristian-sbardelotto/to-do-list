2d2be8060ff80f3b7d4e8c146826a154250d6505
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-04-06 18:38:21 -0300

    feat(task): implementing addTask function

--- src/components/Test/index.tsx
+++ src/components/Test/index.tsx
@@ -1,27 +1,45 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
 
 const Test = () => {
+  const [taskList, setTaskList] = useState([
+    {
+      id: 1,
+      name: 'programming',
+      checked: false,
+    },
+  ]);
+  const [taskValue, setTaskValue] = useState('');
+  const [taskId, setTaskId] = useState(1);
+
+  function addTask() {
+    if (!taskValue) return alert('Type anything in the input');
+
+    setTaskId(taskId + 1);
+
+    const newTask = {
+      id: taskId,
+      name: taskValue,
+      checked: false,
+    };
+
+    setTaskList([...taskList, newTask]);
+  }
+
   return (
     <div>
-      <TestComponent>
-        Out of the span
-        <span>In the span</span>
-      </TestComponent>
+      <input
+        type='text'
+        onChange={e => setTaskValue(e.target.value)}
+      />
+
+      {taskList.map(task => (
+        <div key={task.id}>
+          <span>{task.name}</span>
+          <br />
+        </div>
+      ))}
     </div>
   );
 };
-
-const TestComponent = styled.p`
-  background: red;
-
-  span {
-    background-color: blue;
-
-    &:hover {
-      background-color: aqua;
-    }
-  }
-`;
 
 export default Test;
